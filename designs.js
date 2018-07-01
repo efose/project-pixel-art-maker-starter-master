f782d81d2fdfb63024b9d5864c50148f004b40fb
efose <39982686+efose@users.noreply.github.com>
2018-07-01 20:07:14 +0100

    2nd commit

--- designs.js
+++ designs.js
@@ -5,6 +5,41 @@
 
 function makeGrid() {
 
-// Your code goes here!
+    let height = document.getElementById('inputHeight').value;
+    let width = document.getElementById('inputWidth').value;
+    let table = document.getElementById('pixelCanvas');
+
+    table.innerHTML = '';
+
+    let tbody = document.createElement('tbody');
+
+    for (let i = 0; i < height; i++) {
+
+        let tr = document.createElement('tr');
+
+        for (let j = 0; j < width; j++) {
+
+            let td = document.createElement('td');
+            td.appendChild(document.createTextNode(''));
+            tr.appendChild(td);
+        }
+
+        tbody.appendChild(tr);
+    }
+
+    table.appendChild(tbody);
 
 }
+
+$('body').on('click', 'td', function() {
+
+    let color = document.getElementById('colorPicker').value;
+
+    if ($(this).attr('style')) {
+
+        $(this).removeAttr('style');
+    } else {
+
+        $(this).css('background-color', color);
+    }
+});
